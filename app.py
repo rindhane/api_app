407217c16ed4376e8337d6c0ddb06ba2786013f2
irahul <rindhane@users.noreply.github.com>
2021-08-24 01:47:30 +0530

    add: initiated new version to serve stocks data in json

--- app.py
+++ app.py
@@ -1,104 +1,49 @@
-from flask import Flask ,request , jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from flask import Flask ,request , jsonify, g
 import os
-
+import sqlite3
 #Init app
 app= Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 #Database config
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir,"db.sqlite")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-#Init db
-db=SQLAlchemy(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path=basedir+'/equityBSE.db'
 
-#Init ma 
-ma=Marshmallow(app)
+#database handlers:
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(db_path)
+    return db
 
-#Product Class/Model
-class Product(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(100), unique =True)
-    description =db.Column(db.String(200))
-    price = db.Column(db.Float)
-    qty =db.Column(db.Integer)
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
-    def __init__(self,name,description,price,qty):
-        self.name=name
-        self.description =description
-        self.price=price
-        self.qty=qty
 
-#Product Schema
-class ProductSchema(ma.Schema):
-    class Meta:
-        fields = ("id","name",'description','price','qty')
-
-#Init Schema
-product_schema= ProductSchema()
-products_schema = ProductSchema (many=True)
-
-#create a Product 
-@app.route('/product',methods=['POST'])
-def add_product():
-    name =request.json['name']
-    description =request.json['description']
-    price =request.json['price']
-    qty=request.json['qty']
-    
-    new_product=Product(name, description, price, qty)
-    print(new_product)
-    db.session.add(new_product)
-    db.session.commit()
-    return product_schema.jsonify(new_product)
-
-#Get all products 
-@app.route('/product',methods=['GET'])
-def get_products():
-    all_products=Product.query.all()
-    result =products_schema.dump(all_products)
-    return jsonify(result)
+#Row Factory for resutls from database
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
 
 #Get single product 
-@app.route('/product/<id>',methods=['GET'])
+@app.route('/stocks/<id>',methods=['GET'])
 def get_product(id):
-    product=Product.query.get(id)
-    return product_schema.jsonify(product)
-
-#update a Product 
-@app.route('/product/<id>',methods=['PUT'])
-def update_product(id):
-    product=Product.query.get(id)
-    name =request.json['name']
-    description =request.json['description']
-    price =request.json['price']
-    qty=request.json['qty']
-
-    product.name =name
-    product.description =description
-    product.price= price
-    product.qty =qty
-    
-    #print(product)
-    db.session.commit()
-    return product_schema.jsonify(product)
-
+    try:
+        db= get_db()
+        db.row_factory = make_dicts
+        result=db.execute(f'select * from {id}').fetchall()
+    except sqlite3.OperationalError:
+        result=[]
+    return jsonify(result)
 
 #trial Testing link
 @app.route("/",methods=["GET"])
 def get():
     return jsonify({"msg":'Hello_world'})
 
-#Delete single product 
-@app.route('/product/<id>',methods=['DELETE'])
-def delete_product(id):
-    product=Product.query.get(id)
-    db.session.delete(product)
-    db.session.commit()
-    return product_schema.jsonify(product)
-
-
 #Run Server
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
